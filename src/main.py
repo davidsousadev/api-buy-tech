215b2c2a995e42ffb55b0fb0ff746174858b5894
davidsousadev <davidk1k3k@gmail.com>
2025-02-02 16:07:27 -0300

    Ajuste de CORS

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 # Importação de todos controladores
 from .controllers.emails_controller import router as email_router
@@ -57,7 +58,7 @@ app = create_app()
 if __name__ == "__main__":
     import uvicorn
     import os
-
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])  
     # Porta dinâmica para compatibilidade com serviços do RENDER
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=True)
